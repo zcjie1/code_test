46ba437eb4223bf27c88511dab620f4ed86e4433
Congjie Zhou <zcjie0802@qq.com>
2024-07-01 11:35:00 +0800

    _Bool字节数测试

--- c_test/test.c
+++ c_test/test.c
@@ -1,57 +1,24 @@
-#define _GNU_SOURCE
-#include <dirent.h>     /* Defines DT_* constants */
-#include <fcntl.h>
 #include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/stat.h>
-#include <sys/syscall.h>
+#include <stdbool.h>
 
-#define handle_error(msg) \
-        do { perror(msg); exit(EXIT_FAILURE); } while (0)
-
-struct linux_dirent {
-    long           d_ino;
-    off_t          d_off;
-    unsigned short d_reclen;
-    char           d_name[];
+struct Settings {
+    _Bool offline_disabled : 1;
+    _Bool debug_mode: 1;
+    //unsigned long volume: 4;
+    //bool volume : 4; // 4 bits for volume level (0-15)
 };
 
-#define BUF_SIZE 512
+int main() {
+    struct Settings s = { 
+        .offline_disabled = true, 
+        .debug_mode = false,
+        //.volume = 4
+    };
 
-int main(int argc, char *argv[])
-{
-    int fd, nread;
-    char buf[BUF_SIZE];
-    struct linux_dirent *d;
-    int bpos;
-    char d_type;
-    fd = open(argc > 1 ? argv[1] : "/home/zcj", O_RDONLY | O_DIRECTORY);
-    if (fd == -1)
-        handle_error("open");
-    for ( ; ; ) {
-        nread = syscall(SYS_getdents, fd, buf, BUF_SIZE);
-        if (nread == -1)
-            handle_error("getdents");
-        if (nread == 0)
-            break;
-        printf("--------------- nread=%d ---------------\n", nread);
-        printf("inode#    file_type  d_reclen   d_off                d_name\n");
-        for (bpos = 0; bpos < nread;) {
-            d = (struct linux_dirent *) (buf + bpos);
-            printf("%-8ld  ", d->d_ino);
-            d_type = *(buf + bpos + d->d_reclen - 1);
-            printf("%-10s ", (d_type == DT_REG) ?  "regular" :
-                             (d_type == DT_DIR) ?  "directory" :
-                             (d_type == DT_FIFO) ? "FIFO" :
-                             (d_type == DT_SOCK) ? "socket" :
-                             (d_type == DT_LNK) ?  "symlink" :
-                             (d_type == DT_BLK) ?  "block dev" :
-                             (d_type == DT_CHR) ?  "char dev" : "???");
-            printf("%-10d %-10lld  %-10s\n", d->d_reclen,
-                    (long long) d->d_off, d->d_name);
-            bpos += d->d_reclen;
-        }
-    }
-    exit(EXIT_SUCCESS);
+    printf("size: %ld\n", sizeof(struct Settings));
+    printf("offline_disabled: %d\n", s.offline_disabled);
+    printf("debug_mode: %d\n", s.debug_mode);
+    //printf("volume: %d\n", s.volume);
+    
+    return 0;
 }
