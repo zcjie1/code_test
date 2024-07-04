600ebbf4f8948c141ef04e5a49150d42e2153880
Congjie Zhou <zcjie0802@qq.com>
2024-07-04 09:48:59 +0800

    补遗落文件

--- LKM/UIO_test/simple_user.c
+++ LKM/UIO_test/simple_user.c
@@ -13,38 +13,38 @@
   
 static char uio_addr_buf[18], uio_size_buf[18];
   
-int main(void)  
+int main(void)
 {  
-    int uio_fd, addr_fd, size_fd;  
-    int uio_size;  
+    int uio_fd, addr_fd, size_fd;
+    int uio_size;
     void* uio_addr, *access_address, *new_access_address;
 
     char str[] = "Hello, UIO Device!";
     
     // 打开相关文件
-    uio_fd = open(UIO_DEV, O_RDWR);  
-    addr_fd = open(UIO_ADDR, O_RDONLY);  
-    size_fd = open(UIO_SIZE, O_RDONLY);  
+    uio_fd = open(UIO_DEV, O_RDWR);
+    addr_fd = open(UIO_ADDR, O_RDONLY);
+    size_fd = open(UIO_SIZE, O_RDONLY);
     if( addr_fd < 0 || size_fd < 0 || uio_fd < 0) {
         // close fd 
-        fprintf(stderr, "open error: %s\n", strerror(errno));  
+        fprintf(stderr, "open error: %s\n", strerror(errno));
         exit(-1);  
     }
 
     // 读取uio设备内存起始地址和大小
-    read(addr_fd, uio_addr_buf, sizeof(uio_addr_buf));  
-    read(size_fd, uio_size_buf, sizeof(uio_size_buf));  
-    uio_addr = (void*)strtoul(uio_addr_buf, NULL, 0);  
+    read(addr_fd, uio_addr_buf, sizeof(uio_addr_buf));
+    read(size_fd, uio_size_buf, sizeof(uio_size_buf)); 
+    uio_addr = (void*)strtoul(uio_addr_buf, NULL, 0);
     uio_size = (int)strtoq(uio_size_buf, NULL, 16);
     
     // 映射uio设备文件
     access_address = mmap(NULL, uio_size, PROT_READ | PROT_WRITE, MAP_SHARED, uio_fd, 0);  
     if ( access_address == (void*) -1) {
         // close fd
-        fprintf(stderr, "mmap error: %s\n", strerror(errno));  
+        fprintf(stderr, "mmap error: %s\n", strerror(errno));
         exit(-1);  
     }  
-    printf("The device address: %p (length=%d)\n" 
+    printf("The device address: %p (length=%d)\n"
             "logical address: %p\n", 
             uio_addr, uio_size, access_address);
 
@@ -54,15 +54,15 @@ int main(void)
 
     // mremap测试(需要分配物理内存)
     new_access_address = mremap(access_address, getpagesize(),
-                        getpagesize(), 
+                        getpagesize(),
                         MREMAP_MAYMOVE); 
-    if (new_access_address == (void*) -1) {  
+    if (new_access_address == (void*) -1) {
         fprintf(stderr, "mremmap error: %s\n", strerror(errno));
         munmap(access_address, uio_size + getpagesize() + 1111);
         close(uio_fd);
         close(addr_fd);
         close(size_fd);
-        exit(-1);  
+        exit(-1); 
     }   
     printf(">>>AFTER REMAP: logical address %p\n", new_access_address);
     
