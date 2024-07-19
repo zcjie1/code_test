6fd913f08daba8d5563873af63db78393d818939
Congjie Zhou <zcjie0802@qq.com>
2024-07-19 10:32:14 +0800

    DPDK改名

--- DPDK_test/dpdk/drivers/net/ena/base/ena_defs/ena_gen_info.h
+++
@@ -1,6 +0,0 @@
-/* SPDX-License-Identifier: BSD-3-Clause
- * Copyright (c) 2015-2020 Amazon.com, Inc. or its affiliates.
- * All rights reserved.
- */
-#define	ENA_GEN_DATE	"Thu 14 Apr 2022 12:50:30 PM IDT"
-#define	ENA_GEN_COMMIT	"35388392"
--- DPDK_test/dpdk/dts/framework/__init__.py
+++
@@ -1,3 +0,0 @@
-# SPDX-License-Identifier: BSD-3-Clause
-# Copyright(c) 2022 PANTHEON.tech s.r.o.
-# Copyright(c) 2022 University of New Hampshire
