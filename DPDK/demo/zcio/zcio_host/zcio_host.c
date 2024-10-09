9a091f52e77a1580099b0dcd4f7a46b69d390b5f
zcj <zcjie0802@qq.com>
2024-10-09 14:11:03 +0800

    zcio用户程序

--- DPDK/demo/zcio/zcio_host/zcio_host.c
+++ DPDK/demo/zcio/zcio_host/zcio_host.c
@@ -40,12 +40,16 @@
 #define TX_RING_SIZE 1024
 
 bool force_quit = false;
+uint64_t pkt_num = 0;
 
-#define MAX_PHY_NIC_NUM 32
-struct {
-	int phy_nic_num; // 物理网卡数量
-	uint16_t nic_id[MAX_PHY_NIC_NUM]; // 物理网卡 port_id
-}phy_nic;
+#define MAX_NIC_NUM 16
+struct nic_info{
+	int nic_num; // 网卡数量
+	uint16_t portid[MAX_NIC_NUM]; // 网卡 port_id
+};
+
+static struct nic_info phy_nic;
+static struct nic_info zcio_nic;
 
 struct memory_region {
 	uint64_t host_start_addr;
@@ -272,68 +276,98 @@ static int port_init(uint16_t port, struct rte_mempool *mbuf_pool)
 	if (dev_info.rx_offload_capa & RTE_ETH_RX_OFFLOAD_SCATTER)
 		port_conf.rxmode.offloads |= RTE_ETH_RX_OFFLOAD_SCATTER;
 
-	// 设置网卡对应的ring数量
+	// 初始化网卡驱动
 	retval = rte_eth_dev_configure(port, RX_RING_NUM, TX_RING_NUM, &port_conf);
-	if (retval < 0)
+	if (retval < 0) {
+		printf("Cannot configure device: err=%d, port=%u\n",
+				retval, port);
 		return retval;
+	}
+		
+	// 设置每个ring的描述符数量
+	retval = rte_eth_dev_adjust_nb_rx_tx_desc(port, &nb_rxd, &nb_txd);
+	if (retval < 0) {
+		printf("Cannot adjust number of descriptors: err=%d, port=%u\n",
+				retval, port);
+		return retval;
+	}
+		
 
-	// // 设置每个ring的描述符数量
-	// retval = rte_eth_dev_adjust_nb_rx_tx_desc(port, &nb_rxd, &nb_txd);
-	// if (retval < 0)
-	// 	return retval;
+	// 设置每个receive ring对应的内存池
+	int port_socket = rte_eth_dev_socket_id(port);
+	rxconf = dev_info.default_rxconf;
+	rxconf.offloads = port_conf.rxmode.offloads;
+	for (int r = 0; r < RX_RING_NUM; r++) {
+		retval = rte_eth_rx_queue_setup(port, r, nb_rxd,
+				port_socket, &rxconf, mbuf_pool);
+		if (retval < 0) {
+			printf("Cannot setup receive queue %u for port %u\n",
+					r, port);
+			return retval;
+		}
+			
+	}
 
-	// // 设置每个receive ring对应的内存池
-	// int port_socket = rte_eth_dev_socket_id(port);
-	// rxconf = dev_info.default_rxconf;
-	// rxconf.offloads = port_conf.rxmode.offloads;
-	// for (int r = 0; r < RX_RING_NUM; r++) {
-	// 	retval = rte_eth_rx_queue_setup(port, r, nb_rxd,
-	// 			port_socket, &rxconf, mbuf_pool);
-	// 	if (retval < 0)
-	// 		return retval;
-	// }
+	// 配置发送队列
+	txconf = dev_info.default_txconf;
+	txconf.offloads = port_conf.txmode.offloads;
+	for (int r = 0; r < TX_RING_NUM; r++) {
+		retval = rte_eth_tx_queue_setup(port, r, nb_txd,
+				port_socket, &txconf);
+		if (retval < 0) {
+			printf("Cannot setup transmit queue %u for port %u\n",
+					r, port);
+			return retval;
+		}
+			
+	}
 
-	// // 配置发送队列
-	// txconf = dev_info.default_txconf;
-	// txconf.offloads = port_conf.txmode.offloads;
-	// for (int r = 0; r < TX_RING_NUM; r++) {
-	// 	retval = rte_eth_tx_queue_setup(port, r, nb_txd,
-	// 			port_socket, &txconf);
-	// 	if (retval < 0)
-	// 		return retval;
-	// }
-
-	// retval = rte_eth_dev_set_ptypes(port, RTE_PTYPE_UNKNOWN, NULL, 0);
-	// if (retval < 0)
-	// 	printf("Port %u, Failed to disable Ptype parsing\n", port);
+	retval = rte_eth_dev_set_ptypes(port, RTE_PTYPE_UNKNOWN, NULL, 0);
+	if (retval < 0)
+		printf("Port %u, Failed to disable Ptype parsing\n", port);
 
 	// 启动网卡
 	retval = rte_eth_dev_start(port);
-	if (retval < 0)
+	if (retval < 0) {
+		printf("Cannot start device: err=%d, port=%u\n",
+				retval, port);
 		return retval;
+	}
+		
 	
-	// retval = rte_eth_promiscuous_enable(port);
-	// if (retval < 0)
-	// 	return retval;
+	retval = rte_eth_promiscuous_enable(port);
+	if (retval < 0) {
+		printf("Cannot enable promiscuous mode: err=%d, port=%u\n",
+				retval, port);
+		return retval;
+	}
+		
 
-	// // 输出网卡信息
-	// struct rte_ether_addr addr;
-	// retval = rte_eth_macaddr_get(port, &addr);
-	// if (retval < 0)
-	// 	return retval;
+	// 输出网卡信息
+	struct rte_ether_addr addr;
+	retval = rte_eth_macaddr_get(port, &addr);
+	if (retval < 0) {
+		printf("Error reading MAC address for port %d\n", port);
+		return retval;
+	}
 
-	// printf("Port %u: \n", port);
-    // printf("    MAC: %02"PRIx8 ":" "%02"PRIx8 ":" "%02"PRIx8 ":"
-	// 		   "%02"PRIx8 ":" "%02"PRIx8 ":" "%02"PRIx8 "\n",
-	// 		RTE_ETHER_ADDR_BYTES(&addr));
+	printf("Port %u: \n", port);
+    printf("    MAC: %02"PRIx8 ":" "%02"PRIx8 ":" "%02"PRIx8 ":"
+			   "%02"PRIx8 ":" "%02"PRIx8 ":" "%02"PRIx8 "\n",
+			RTE_ETHER_ADDR_BYTES(&addr));
     
     rte_eth_dev_get_name_by_port(port, device_name);
     printf("    Device Name: %s\n", device_name);
     printf("    Driver Name: %s\n\n", dev_info.driver_name);
 
-	// 统计普通物理网卡数量
-	phy_nic.nic_id[phy_nic.phy_nic_num] = port;
-	phy_nic.phy_nic_num++;
+	// 统计网卡数量
+	if(strcmp(dev_info.driver_name, "net_zcio") == 0) {
+		zcio_nic.portid[zcio_nic.nic_num] = port;
+		zcio_nic.nic_num++;
+	}else {
+		phy_nic.portid[phy_nic.nic_num] = port;
+		phy_nic.nic_num++;
+	}
 }
 
 // 信号处理函数
@@ -344,6 +378,94 @@ static void signal_handler(int signum)
 				signum);
 		force_quit = true;
 	}
+}
+
+static struct rte_mbuf* generate_testpkt(struct rte_mempool *mbuf_pool)
+{
+	struct rte_mbuf *pkt = rte_pktmbuf_alloc(mbuf_pool);
+	const struct rte_ether_addr src_mac = {
+		.addr_bytes = { 0x12, 0x34, 0x56, 0x78, 0x9a, 0xbc }
+	};
+	const struct rte_ether_addr dst_mac = { 
+		.addr_bytes = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff } 
+	};
+
+	// Meta data 初始化
+	pkt->data_len = 14 + 8; // MAC + Message
+	pkt->data_off = RTE_PKTMBUF_HEADROOM;
+	pkt->pkt_len = 14 + 8;
+	pkt->nb_segs = 1;
+	pkt->l2_len	= sizeof(struct rte_ether_hdr);
+	pkt->l3_len	= 0;
+	pkt->next = NULL;
+
+	// 二层初始化
+	struct rte_ether_hdr *eth_hdr = rte_pktmbuf_mtod(pkt, struct rte_ether_hdr*);
+	rte_ether_addr_copy(&src_mac, &eth_hdr->src_addr);
+	rte_ether_addr_copy(&dst_mac, &eth_hdr->dst_addr);
+	// eth_hdr->ether_type = ((uint16_t)0x0008);
+	eth_hdr->ether_type = rte_cpu_to_be_16((uint16_t)0x0800);
+	uint64_t *data = (uint64_t *)(eth_hdr + 1);
+	*data = rte_cpu_to_be_64((uint64_t)114514);
+	
+	return pkt;
+}
+
+static void parse_pkt(struct rte_mbuf *pkt)
+{
+	struct rte_ether_addr src_mac, dst_mac;
+	uint16_t ether_type;
+	
+	struct rte_ether_hdr *eth_hdr = rte_pktmbuf_mtod(pkt, struct rte_ether_hdr*);
+	src_mac = eth_hdr->src_addr;
+	dst_mac = eth_hdr->dst_addr;
+	ether_type = eth_hdr->ether_type;
+	uint64_t *data_ptr = (uint64_t *)(eth_hdr + 1);
+	uint64_t raw_data = *data_ptr;
+	uint64_t data = rte_be_to_cpu_64(raw_data);
+	printf("--------------------------PACKET %lu--------------------------\n", pkt_num++);
+	printf("src mac %02"PRIx8 ":%02"PRIx8 ":%02"PRIx8 ":%02"PRIx8 ":%02"PRIx8 ":%02"PRIx8 "\n",
+		RTE_ETHER_ADDR_BYTES(&src_mac));
+	printf("dst mac %02"PRIx8 ":%02"PRIx8 ":%02"PRIx8 ":%02"PRIx8 ":%02"PRIx8 ":%02"PRIx8 "\n",
+		RTE_ETHER_ADDR_BYTES(&dst_mac));
+	printf("ether type %04"PRIx16 "\n", ether_type);
+	printf("raw data %016"PRIx64 "\n", raw_data);
+	printf("data %016"PRIx64 "\n", data);
+	printf("--------------------------------------------------------------\n");
+	*data_ptr = rte_cpu_to_be_64(data+1);
+}
+
+static int server_test(void *arg)
+{
+	struct rte_mempool *mbuf_pool = arg;
+	if(zcio_nic.nic_num != 1) {
+		printf("Error: The number of zcio nic is not 1\n");
+		return -1;
+	}
+	
+	int ret = 0;
+	struct rte_mbuf *pkt = generate_testpkt(mbuf_pool);
+	ret = rte_eth_tx_burst(zcio_nic.portid[0], 0, &pkt, 1);
+	while(!ret && !force_quit)
+		ret = rte_eth_tx_burst(zcio_nic.portid[0], 0, &pkt, 1);
+	
+	struct rte_mbuf *recv;
+	
+	while(!force_quit) {
+		ret = rte_eth_rx_burst(zcio_nic.portid[0], 0, &recv, 1);
+		if(ret) {
+			parse_pkt(recv);
+			sleep(1);
+			ret = rte_eth_tx_burst(zcio_nic.portid[0], 0, &recv, 1);
+			while(!ret && !force_quit)
+				ret = rte_eth_tx_burst(zcio_nic.portid[0], 0, &recv, 1);
+			continue;
+		}
+		usleep(100);
+	}
+	
+	rte_pktmbuf_free(pkt);
+	return 0;
 }
 
 int main(int argc, char *argv[])
@@ -384,15 +506,22 @@ int main(int argc, char *argv[])
 
     // 初始化网卡
 	RTE_ETH_FOREACH_DEV(portid)
-		port_init(portid, mbuf_pool);
-		// if (port_init(portid, mbuf_pool) != 0)
-		// 	rte_exit(EXIT_FAILURE, "Cannot init port %"PRIu16 "\n", portid);
-
+		if (port_init(portid, mbuf_pool) != 0) {
+			force_quit = true;
+			printf("\nCannot init port %"PRIu16"\n", portid);
+			goto out;
+		}
+		
 	printf("\nStart Processing...\n\n");
+
+	// 分配工作核心任务
+	worker_id = rte_get_next_lcore(worker_id, 1, 0);
+	rte_eal_remote_launch(server_test, mbuf_pool, worker_id);
 	
 	signal(SIGINT, signal_handler);
 	signal(SIGTERM, signal_handler);
 
+ out:
 	// 等待工作核心结束任务
 	rte_eal_mp_wait_lcore();
 
